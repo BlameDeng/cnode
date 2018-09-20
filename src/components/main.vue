590c49472df67e2732486c636ad614b9a6faee87
BlameDeng <476961779@qq.com>
2018-09-20 22:22:24 +0800

    没有解决

--- src/components/main.vue
+++ src/components/main.vue
@@ -21,13 +21,17 @@
                                 <p class="topic-info-inner-tab" v-if="topic.good">精华</p>
                                 <p class="topic-info-inner-tab-default" v-if="!topic.top&&!topic.good">{{topic.tab|tab}}</p>
                                 <p class="topic-info-inner-title" :title="topic.title">
-                                    <router-link :to="`./topic/${topic.id}`">{{topic.title}}</router-link>
+                                    <router-link :to="`/topic/${topic.id}`">{{topic.title}}</router-link>
                                 </p>
                             </div>
-                            <p class="topic-info-count"><span>{{topic.reply_count}}/</span><span>{{topic.visit_count}}</span></p>
+                            <p class="topic-info-count">
+                                <span>{{topic.reply_count}}/</span>
+                                <span>{{topic.visit_count}}</span>
+                            </p>
                         </div>
                         <p class="topic-reply">
-                            <router-link :to="`./topic/${topic.id}`"><span>最新回复：</span>{{$formatDate(topic.last_reply_at).des}}</router-link>
+                            <router-link :to="`/topic/${topic.id}`">
+                                <span>最新回复：</span>{{$formatDate(topic.last_reply_at).des}}</router-link>
                         </p>
                     </div>
                     <div class="pagination-wrapper">
@@ -43,229 +47,231 @@
     </div>
 </template>
 <script>
-    import Row from './row'
-    import Col from './col'
-    import Pagination from './pagination/pagination'
-    import { mapActions } from 'vuex'
-    export default {
-        name: 'cMain',
-        components: {
-            'x-row': Row,
-            'x-col': Col,
-            'x-pagination': Pagination
-        },
-        data() {
-            return {
-                topics: [],
-                query: { tab: 'all', page: 1 }
-            }
-        },
-        filters: {
-            tab(val) {
-                if (val === "share") {
-                    return '分享';
-                } else if (val === "ask") {
-                    return "问答";
-                } else if (val === "job") {
-                    return "招聘";
-                } else if (val === "good") {
-                    return "精华";
-                }
-                return '分享';
-            }
-        },
-        methods: {
-            ...mapActions(['getTopics', 'getTopicById']),
-            onClickTab(e) {
-                this.query.tab = e.target.getAttribute('data-tab');
-            }
-        },
-        created() {
-            let link = this.$route.query;
-            if (link) {
-                this.getTopics({ ...{ link }, limit: 20 }).then(res => {
-                    this.topics = res.data;
-                    this.$router.push({ path: '/', query: { ...{ link } } });
-                }).catch(err => { console.log(err); })
-            }
-        },
-        watch: {
-            query: {
-                handler(val) {
-                    this.getTopics({ page: val.page, tab: val.tab, limit: 20 }).then(res => {
-                        this.topics = res.data;
-                        this.$router.push({ path: '/', query: { tab: val.tab, page: val.page } });
-                    }).catch(err => { console.log(err); })
-                },
-                deep: true,
-                immediate: true
-            }
-        }
+import Row from "./row";
+import Col from "./col";
+import Pagination from "./pagination/pagination";
+import { mapActions } from "vuex";
+export default {
+  name: "cMain",
+  components: {
+    "x-row": Row,
+    "x-col": Col,
+    "x-pagination": Pagination
+  },
+  data() {
+    return {
+      topics: [],
+      query: { tab: "all", page: 1 }
+    };
+  },
+  filters: {
+    tab(val) {
+      if (val === "share") {
+        return "分享";
+      } else if (val === "ask") {
+        return "问答";
+      } else if (val === "job") {
+        return "招聘";
+      } else if (val === "good") {
+        return "精华";
+      }
+      return "分享";
     }
+  },
+  methods: {
+    ...mapActions(["getTopics", "getTopicById"]),
+    onClickTab(e) {
+      this.query.tab = e.target.getAttribute("data-tab");
+    }
+  },
+  created() {
+    this.getTopics({ page: 1 })
+      .then(res => {
+        this.topics = res.data;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  },
+  watch: {
+    query: {
+      handler(val) {
+        this.getTopics({ page: val.page, tab: val.tab, limit: 20 })
+          .then(res => {
+            this.topics = res.data;
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      },
+      deep: true
+    }
+  }
+};
 </script>
 <style lang="scss" scoped>
-    $nodegreen:#80bd01;
-    $error:#F56C6C;
-    $warning:#E6A03C;
-    $tips:#919196;
-    .c-main {
-        margin: 20px 0;
-        border-radius: 4px;
-        .main-content {
-            margin: 0 10px;
-            &-navbar {
-                border-top-left-radius: 4px;
-                border-top-right-radius: 4px;
-                >ul {
-                    border-top-left-radius: 4px;
-                    border-top-right-radius: 4px;
-                    display: flex;
-                    justify-content: flex-start;
-                    align-items: center;
-                    background: #eee;
-                    color: $nodegreen;
-                    font-size: 14px;
-                    padding: 10px;
-                    >li {
-                        margin-right: 20px;
-                        padding: 2px 6px;
-                        border-radius: 4px;
-                        cursor: pointer;
-                        &:hover {
-                            color: #9e78c0;
-                        }
-                        &.active {
-                            background: $nodegreen;
-                            color: #fff;
-                        }
-                    }
-                }
-            }
-            .topic {
-                display: flex;
-                justify-content: flex-start;
-                align-items: center;
-                border-bottom: 1px solid #eee;
-                padding: 5px 0;
-                background: #fff;
-                &:hover {
-                    background: #eee;
-                }
-                >img {
-                    width: 30px;
-                    height: 30px;
-                    margin: 10px;
-                    border-radius: 4px;
-                    cursor: pointer;
-                }
-                &-info {
-                    display: flex;
-                    justify-content: flex-start;
-                    align-items: center;
-                    flex-wrap: wrap;
-                    max-width: 70%;
-                    &-inner {
-                        display: flex;
-                        justify-content: flex-start;
-                        align-items: center;
-                        width: 100%;
-                        order: 1;
-                        &-tab {
-                            font-size: 12px;
-                            background: $nodegreen;
-                            color: #fff;
-                            padding: 2px 5px;
-                            border-radius: 3px;
-                            margin: 0 5px 0 0;
-                            flex-shrink: 0;
-                        }
-                        &-tab-default {
-                            font-size: 12px;
-                            background: #ddd;
-                            color: #888;
-                            padding: 2px 5px;
-                            border-radius: 3px;
-                            margin: 0 5px 0 0;
-                            flex-shrink: 0;
-                        }
-                        &-title {
-                            font-size: 16px;
-                            white-space: nowrap;
-                            overflow: hidden;
-                            text-overflow: ellipsis;
-                            cursor: pointer;
-                            &:hover {
-                                text-decoration: underline;
-                            }
-                        }
-                    }
-                    &-count {
-                        order: 2;
-                        width: 70px;
-                        margin-left: 40px;
-                        text-align: start;
-                        flex-shrink: 0;
-                        >span:first-child {
-                            font-size: 14px;
-                            color: #9e78c0;
-                        }
-                        >span:last-child {
-                            font-size: 12px;
-                            color: #999;
-                        }
-                    }
-                }
-                &-reply {
-                    margin-left: auto;
-                    flex-shrink: 0;
-                    font-size: 12px;
-                    padding-right: 10px;
-                    cursor: pointer;
-                    span {
-                        display: none;
-                    }
-                }
-            }
-            .pagination-wrapper {
-                background: #fff;
-                padding: 10px;
-                border-bottom-left-radius: 4px;
-                border-bottom-right-radius: 4px;
-            }
+$nodegreen: #80bd01;
+$error: #f56c6c;
+$warning: #e6a03c;
+$tips: #919196;
+.c-main {
+  margin: 20px 0;
+  border-radius: 4px;
+  .main-content {
+    margin: 0 10px;
+    &-navbar {
+      border-top-left-radius: 4px;
+      border-top-right-radius: 4px;
+      > ul {
+        border-top-left-radius: 4px;
+        border-top-right-radius: 4px;
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        background: #eee;
+        color: $nodegreen;
+        font-size: 14px;
+        padding: 10px;
+        > li {
+          margin-right: 20px;
+          padding: 2px 6px;
+          border-radius: 4px;
+          cursor: pointer;
+          &:hover {
+            color: #9e78c0;
+          }
+          &.active {
+            background: $nodegreen;
+            color: #fff;
+          }
         }
-        .sider-bar {
-            display: none;
-        }
-        @media (min-width:993px) {
-            .main-content {
-                margin: 0 10px;
-                .topic {
-                    >img {
-                        margin: 10px 0 10px 10px;
-                    }
-                    &-info {
-                        flex-wrap: nowrap;
-                        &-inner {
-                            width: 90%;
-                            order: 2;
-                        }
-                        &-count {
-                            order: 1;
-                            margin-left: 0px;
-                            text-align: center;
-                        }
-                    }
-                    &-reply {
-                        span {
-                            display: inline;
-                        }
-                    }
-                }
-            }
-            .sider-bar {
-                display: block;
-                border: 1px solid red;
-            }
-        }
+      }
     }
+    .topic {
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+      border-bottom: 1px solid #eee;
+      padding: 5px 0;
+      background: #fff;
+      &:hover {
+        background: #eee;
+      }
+      > img {
+        width: 30px;
+        height: 30px;
+        margin: 10px;
+        border-radius: 4px;
+        cursor: pointer;
+      }
+      &-info {
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        flex-wrap: wrap;
+        max-width: 70%;
+        &-inner {
+          display: flex;
+          justify-content: flex-start;
+          align-items: center;
+          width: 100%;
+          order: 1;
+          &-tab {
+            font-size: 12px;
+            background: $nodegreen;
+            color: #fff;
+            padding: 2px 5px;
+            border-radius: 3px;
+            margin: 0 5px 0 0;
+            flex-shrink: 0;
+          }
+          &-tab-default {
+            font-size: 12px;
+            background: #ddd;
+            color: #888;
+            padding: 2px 5px;
+            border-radius: 3px;
+            margin: 0 5px 0 0;
+            flex-shrink: 0;
+          }
+          &-title {
+            font-size: 16px;
+            white-space: nowrap;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            cursor: pointer;
+            &:hover {
+              text-decoration: underline;
+            }
+          }
+        }
+        &-count {
+          order: 2;
+          width: 70px;
+          margin-left: 40px;
+          text-align: start;
+          flex-shrink: 0;
+          > span:first-child {
+            font-size: 14px;
+            color: #9e78c0;
+          }
+          > span:last-child {
+            font-size: 12px;
+            color: #999;
+          }
+        }
+      }
+      &-reply {
+        margin-left: auto;
+        flex-shrink: 0;
+        font-size: 12px;
+        padding-right: 10px;
+        cursor: pointer;
+        span {
+          display: none;
+        }
+      }
+    }
+    .pagination-wrapper {
+      background: #fff;
+      padding: 10px;
+      border-bottom-left-radius: 4px;
+      border-bottom-right-radius: 4px;
+    }
+  }
+  .sider-bar {
+    display: none;
+  }
+  @media (min-width: 993px) {
+    .main-content {
+      margin: 0 10px;
+      .topic {
+        > img {
+          margin: 10px 0 10px 10px;
+        }
+        &-info {
+          flex-wrap: nowrap;
+          &-inner {
+            width: 90%;
+            order: 2;
+          }
+          &-count {
+            order: 1;
+            margin-left: 0px;
+            text-align: center;
+          }
+        }
+        &-reply {
+          span {
+            display: inline;
+          }
+        }
+      }
+    }
+    .sider-bar {
+      display: block;
+      border: 1px solid red;
+    }
+  }
+}
 </style>
